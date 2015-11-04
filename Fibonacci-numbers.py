77cdd8a3c40c8b042ba5d74740714cff50873eae
Michał Pańczyk <mjpanczyk@gmail.com>
2015-11-04 22:52:16 +0100

    Some refactoring

--- Fibonacci-numbers.py
+++ Fibonacci-numbers.py
@@ -13,49 +13,38 @@ Options
 
 """
 
-import sys
-
-arg = sys.argv
+from sys import argv, exit
 
 def fibonacci(n):
-	global Fn, Fn_1, count
-	Fn = 1.0
-	Fn_1 = 1.0
-	count = 2
-	while True:
-		count = count + 1
-		Fn = Fn + Fn_1
-		Fn_1 = Fn - Fn_1
-		gR = Fn/Fn_1
+	Fn_1, Fn = 0, 1
+	FibonacciSequence = [Fn_1, Fn]
+	goldenRatio = [None]
+	for i in range(n):
+		Fn_1, Fn = Fn, Fn_1+Fn
 		FibonacciSequence.append(Fn)
-		goldenRatio.append(gR)
-		if count == n:
-			break
+		goldenRatio.append(float(Fn)/Fn_1)
+	return FibonacciSequence, goldenRatio
 
-while len(arg) > 1:
+if len(argv) > 1:
 	print(__doc__)
-	sys.exit(0)
+	exit(0)
 
 while True:
 	number = raw_input("Enter the number, n to obtain the Fibonacci Sequence: ")
 	try:
-		num = float(number)
+		num = int(number)
 		if num > 100:
 			print "Enter a value less or equal to 100"
 			continue
-		print "Your entered number is: %.1f\n" % (num)
+		print "The number you have entered is: %d" % num
 		break
-	except:
-		print "Error:Enter only numbers"
+	except ValueError:
+		print "Error: Enter only numbers"
 		continue
 
-FibonacciSequence = [1.0]
-goldenRatio = []
+FibonacciSequence, goldenRatio = fibonacci(num)
 
-fibonacci(num)
-
-
-print "Fibonacci Sequence for the value n = %.1f\n" %(num)
+print "Fibonacci Sequence for the value n = %d" % num
 print FibonacciSequence
 print "\nGolden Ratio\n"
 print goldenRatio
